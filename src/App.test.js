c5515aa2fb4081c9d7b6687672b69b2c56fabe23
Nicolas Diaz <nicolasd@stensul.com>
2018-10-30 10:42:02 -0300

    Break test pipeline 2

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('xiv');
-  ReactDOM.render(<App />, div);
+  ReactDO.render(<App />, div);
 });
